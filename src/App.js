0d8e1fe19d6e1e0d77ee66a3e28f819d1f0830eb
leydaaltun <altunleyda@gmail.com>
2024-09-02 20:46:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Counter />
     </div>
-  );
+  )
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1); 
+  const [count, setCount] = useState(0);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1)
+  }
+  const date = new Date("June 21 2027");
+  date.setDate(date.getDate() + count);
+
+
+  return(
+    <>
+      <div className="button-wrapper">
+        <input type="range" min="0" max="10" value={step} onChange={e => setStep(Number(e.target.value))} />
+        <span>Step: {step}</span>
+      </div>
+      <div className="button-wrapper"> 
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input type="text" value={count} onChange={e => setCount(Number(e.target.value))} />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>{count === 0 ? `Today is` : count > 0 ? `${count} days from today is` : `${Math.abs(count)} days ago was`}</span>
+        {date.toDateString()}</p>
+      {(count !== 0  || step !== 1) ? <div>
+        <button onClick={handleReset}>Reset</button>
+      </div> : null}
+    </>
+  )
+}
